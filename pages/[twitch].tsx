752d300adefc1cdc38b9db3645c0f3e390edf757
lumini23 <lumini2@hotmail.com.br>
2022-11-07 01:38:09 -0300

    teste 3

--- pages/[twitch].tsx
+++ pages/[twitch].tsx
@@ -1,9 +1,14 @@
 import { TwitchEmbed } from "react-twitch-embed"
 import { useRouter } from 'next/router'
 import { useState, useEffect } from 'react'
-import { CREDENTIAL } from '../credentials'
 import axios from "axios"
-import { collapseTextChangeRangesAcrossMultipleVersions } from "typescript"
+import cookieParser from "cookie-parser"
+import { access } from "fs"
+import { CREDENTIAL } from "../credentials"
+
+const getCookie = (cookie: string, parameter: string) => {
+  return cookie.split(parameter + '=')[1].split(';')[0]
+}
 
 export default function Twitch() {
   const qs = require('qs');
@@ -17,38 +22,14 @@ export default function Twitch() {
 
   useEffect(() => {
     const token  = router.asPath.split('#')[1]?.split('=')[1].split('&')[0];
-    console.log(token);
     if (token) {
-      document.cookie = '';
-      document.cookie = token
-      //axios({
-      //  method: 'post',
-      //  url: 'https://id.twitch.tv/oauth2/token',
-      //  headers: { 'content-type': 'application/x-www-form-urlencoded' },
-      //  data: data
-      //}).then (response => {
-      //  console.log(response)
-      //})
-    }
+      document.cookie = 'access_token='+token
+    } 
   }, [router.asPath])
 
-  useEffect(() => {
-    console.log(document.cookie)
-    if (document.cookie) {
-      axios({
-        method: 'get',
-        url: 'https://id.twitch.tv/oauth2/validate',
-        headers: {
-          'Authorization': 'OAuth' + document.cookie.split(';')[1]
-        }
-      }).then(response => {
-        console.log(response)
-      })
-    }
-  }, [])
 
   const handleFetch = () => {
-    location.href = 'https://id.twitch.tv/oauth2/authorize?scope=channel%3Amanage%3Apolls+channel%3Aread%3Apolls&response_type=token&client_id=hr749j5n096z7829cjqgr70z1w62b9&redirect_uri=http://localhost:3000'
+    location.href = `https://id.twitch.tv/oauth2/authorize?scope=channel%3Amanage%3Apolls+channel%3Aread%3Apolls+user%3Aread%3Afollows&response_type=token&client_id=${CREDENTIAL.client_id}&redirect_uri=${CREDENTIAL.redirect_uri}`
   }   
 
   return (
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,5 +1,3 @@
-import { TwitchPlayer, TwitchChat } from "react-twitch-embed"
-import { useRouter } from 'next/router'
 import Twitch from "./[twitch]";
 
 export default function Home() {
