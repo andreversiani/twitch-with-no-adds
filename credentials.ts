4642127f6b83b0b3aab67e19a0f7103f0a4b13d4
lumini23 <lumini2@hotmail.com.br>
2022-11-07 00:49:58 -0300

    teste

---
+++ credentials.ts
@@ -0,0 +1,5 @@
+export const CREDENTIAL = {
+  client_id: 'hr749j5n096z7829cjqgr70z1w62b9',
+  client_secret: 'wyshg0km05mbedqdytjsnlm1sxj0ui',
+  redirect_uri: 'http://localhost:3000',
+}
--- pages/[twitch].tsx
+++ pages/[twitch].tsx
@@ -1,25 +1,64 @@
-import { TwitchPlayer, TwitchChat } from "react-twitch-embed"
+import { TwitchEmbed } from "react-twitch-embed"
 import { useRouter } from 'next/router'
-import { useState } from 'react'
-import { CentralizedFlexContainer, FlexContainer  } from "../styles/styles";
+import { useState, useEffect } from 'react'
+import { CREDENTIAL } from '../credentials'
+import axios from "axios"
+import { collapseTextChangeRangesAcrossMultipleVersions } from "typescript"
 
 export default function Twitch() {
+  const qs = require('qs');
   const router = useRouter();
   const [input, setInput] = useState('');
-  const channelName = router.query.twitch;
+  const channelName = router?.query?.twitch;
 
   const handleSearch = () => {
     router.push(input)
   }
 
+  useEffect(() => {
+    const token  = router.asPath.split('#')[1]?.split('=')[1].split('&')[0];
+    console.log(token);
+    if (token) {
+      document.cookie = '';
+      document.cookie = token
+      //axios({
+      //  method: 'post',
+      //  url: 'https://id.twitch.tv/oauth2/token',
+      //  headers: { 'content-type': 'application/x-www-form-urlencoded' },
+      //  data: data
+      //}).then (response => {
+      //  console.log(response)
+      //})
+    }
+  }, [router.asPath])
+
+  useEffect(() => {
+    console.log(document.cookie)
+    if (document.cookie) {
+      axios({
+        method: 'get',
+        url: 'https://id.twitch.tv/oauth2/validate',
+        headers: {
+          'Authorization': 'OAuth' + document.cookie.split(';')[1]
+        }
+      }).then(response => {
+        console.log(response)
+      })
+    }
+  }, [])
+
+  const handleFetch = () => {
+    location.href = 'https://id.twitch.tv/oauth2/authorize?scope=channel%3Amanage%3Apolls+channel%3Aread%3Apolls&response_type=token&client_id=hr749j5n096z7829cjqgr70z1w62b9&redirect_uri=http://localhost:3000'
+  }   
+
   return (
-    <>
-        <div style={{display: 'flex', alignItems: 'space-between', justifyContent: 'center'}}>
-          <div style={{width: '100%', display: 'flex', alignItems: 'center', justifyContent: 'center'}}>
-            <TwitchPlayer width={'100%'} height={'100%'} channel={channelName as string} />
-          </div>
-          <TwitchChat style={{height: '100vh'}} channel={channelName as string} />
+    <>  {channelName ?
+        <div style={{height: '100vh'}}>
+          <TwitchEmbed width={'100%'} height={'100%'} channel={channelName as string} />
         </div>
+        :
+        <button onClick={handleFetch}>CLICK</button>
+        }
     </>
   )
 }
